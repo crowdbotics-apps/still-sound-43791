549081f245ee9527fe53cfb5eae98e84e86e4bd3
Crowdbotics <team@crowdbotics.com>
2023-10-06 01:49:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-sound-43791",
+  url: "https://still-sound-43791.botics.co",
+  api: "https://still-sound-43791.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
